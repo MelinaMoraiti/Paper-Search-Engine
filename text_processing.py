eafdac84f04ea693c4ae21d2d53a7ef7f23296b2
Melina Moraiti <melinamoraith@gmail.com>
2023-12-18 09:28:21 +0200

    add preprocess_paper function

--- text_processing.py
+++ text_processing.py
@@ -1,6 +1,6 @@
 # text_processing.py
 import nltk
-import string
+import string,re
 from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -13,19 +13,36 @@ def tokenize_text(text):
     tokens = word_tokenize(text)
     return tokens
 # Sanitizes text from punctuation and stopwords
-def clean_text(text):
-    stopwords = nltk.corpus.stopwords.words('english')
-    tokens = tokenize_text(text)
+def clean_tokens(tokens):
+    stop_words = set(stopwords.words('english'))
     cleaned_tokens =[]
     for token in tokens:
-        if token not in string.punctuation and stopwords:
+        if token not in string.punctuation and token not in stop_words:
             cleaned_tokens.append(token)
     return cleaned_tokens
-def normalize_text(text):
-    lemmatizer = WordNetLemmatizer()
-    #stemmer = PorterStemmer()
-    tokens = clean_text(text)
-    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
-    #stemmed_tokens = [stemmer.stem(token) for token in tokens]
-    return lemmatized_tokens
-   
+def normalize_tokens(tokens,option):
+    if option == 'l':
+        lemmatizer = WordNetLemmatizer()
+        normalized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    elif option== 's':
+        stemmer = PorterStemmer()
+        normalized_tokens = [stemmer.stem(token) for token in tokens]
+    else:
+        raise ValueError("Invalid option. Use 'l' for lemmatization or 's' for stemming.")
+    return normalized_tokens
+def preprocess_paper(paper):
+    text_content = f"{paper['Title']} {paper['Authors']} {paper['Abstract']} {paper['Subject_Tags']} {paper['Subjects']} {paper['Submitted Date']}"
+
+    # Tokenize the text
+    tokens = tokenize_text(text_content)
+    
+    # Clean the tokens from punctuation and stopwords
+    cleaned_tokens = clean_tokens(tokens)
+
+    # Normalize tokens based on the specified option ('l' for lemmatization, 's' for stemming)
+    normalized_tokens = normalize_tokens(cleaned_tokens, option='l')
+    # Join tokens back into a clean text
+    cleaned_text = ' '.join(normalized_tokens)
+
+    return cleaned_text
+
