718b3cc814e59f874eddd12dccca2185e2ec2711
Melina Moraiti <melinamoraith@gmail.com>
2023-12-24 16:53:31 +0200

    Update Boolean Operations

--- sources/QueryProcessor.py
+++ sources/QueryProcessor.py
@@ -3,12 +3,29 @@ class QueryProcessor:
     def __init__(self, inverted_index):
         self.inverted_index = inverted_index
     def process_query(self, user_query):
-        processed_query = preprocess_text(user_query)
-        processed_query_tokens = tokenize_text(user_query)
-        results = self.boolean_operations(processed_query_tokens)
+        query_tokens = tokenize_text(user_query) # Only tokenize the user query
+        results = self.boolean_retrieval(query_tokens)
         return results
-    def boolean_operations(self, processed_query_tokens):
-        result_documents = set(self.inverted_index.index.keys())
+    def boolean_retrieval(self,processed_query_tokens):
+        current_documents = None # initialize current docs with None
+        current_operator = 'AND'  # Default to AND
         for token in processed_query_tokens:
-            result_documents = set.intersection(set(self.inverted_index.index[token]))
+            if token.upper() in {'AND', 'OR', 'NOT'}:
+                # Update the current operator
+                current_operator = token.upper()
+            else:
+                term_documents = set(self.inverted_index.index.get(token, {}).keys())
+                 # Update the set of current documents based on the current operator
+                if current_operator == 'AND':
+                     # If this is the first term, set current_documents to the term_documents
+                    if current_documents is None:
+                        current_documents = term_documents
+                    else:
+                        current_documents = current_documents.intersection(term_documents)
+                    current_documents = current_documents.intersection(term_documents)
+                elif current_operator == 'OR':
+                    current_documents = current_documents.union(term_documents)
+                elif current_operator == 'NOT':
+                    current_documents = current_documents.difference(term_documents)
+        result_documents = current_documents 
         return result_documents
--- sources/main.py
+++ sources/main.py
@@ -1,21 +1,21 @@
 import paper_crawler
 import file_operations
-from text_processing import preprocess_paper
+from text_processing import preprocess_paper,preprocess_text
 from InvertedIndex import InvertedIndex
 from QueryProcessor import QueryProcessor
-'''
+"""
 categories = ['cs','cond-mat','astro-ph','math','q-bio','q-fin','gr-qc','hep-ex','hep-lat','hep-ph','hep-th','math-ph','nlin','nucl-ex','nucl-th','physics','quant-ph','stat','econ']  
 #query = ['Distributed Systems']
 
-target_urls = paper_crawler.create_urls(categories,200,1000)
+target_urls = paper_crawler.create_urls(categories,50,50)
 papers_data = paper_crawler.crawl_websites(target_urls)
 # Saving Data in files.
-file_operations.save_data(papers_data,'/files/arXiv_papers.json')
-file_operations.save_data(papers_data,'/files/arXiv_papers.csv')
-'''
+file_operations.save_data(papers_data,'../files/arXiv_papers_less.json')
+file_operations.save_data(papers_data,'../files/arXiv_papers_less.csv')
 
+"""
 # load data from file
-papers_collection = file_operations.retrieve_data('../files/arXiv_papers.json')
+papers_collection = file_operations.retrieve_data('../files/arXiv_papers_less.json')
 # text_processing
 preprocessed_texts = {}
 for paper in papers_collection:
@@ -27,12 +27,12 @@ inverted_index = InvertedIndex()
 # Add papers to the inverted index
 for doc_id, preprocessed_text in preprocessed_texts.items():
     inverted_index.add_document(doc_id, preprocessed_text)
-terms=["cs.IT"]
-inverted_index.print(terms)
 
-user_query = "cs.IT"
+user_query = "SymTFT OR C++"
+
 query_processor = QueryProcessor(inverted_index)
 result_docs = query_processor.process_query(user_query)
-print("Matching Documents:", result_docs)
-
-
+if result_docs:
+    print(f"Matching Documents for {user_query}:", result_docs)
+else:
+    print("No Matches")
