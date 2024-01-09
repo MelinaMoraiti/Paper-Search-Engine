2c906966ad4c578b504a2702415b55cb1e03e138
Melina Moraiti <melinamoraith@gmail.com>
2024-01-09 23:03:16 +0200

    Preprocess paper metadata

--- sources/text_processing.py
+++ sources/text_processing.py
@@ -6,9 +6,7 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 def tokenize_text(text):
-    # Lowercasing
     text = text.lower()
-    # Tokenization
     tokens = word_tokenize(text)
     return tokens
 # Sanitizes text from punctuation and stopwords
@@ -30,13 +28,12 @@ def normalize_tokens(tokens,option):
         raise ValueError("Invalid option. Use 'l' for lemmatization or 's' for stemming.")
     return normalized_tokens
 def preprocess_text(text):
-    tokens=tokenize_text(text)
+    tokens = tokenize_text(text)
     cleaned_tokens = clean_tokens(tokens)
-    normalized_tokens=normalize_tokens(cleaned_tokens, option='l')
-   # cleaned_text = ''.join(normalized_tokens)
-    return normalized_tokens
-
+    normalized_tokens = normalize_tokens(cleaned_tokens, option='l')
+    cleaned_text = ' '.join(normalized_tokens)
+    return cleaned_text
 def preprocess_paper(paper):
     text_paper_metadata = f"{paper['Title']} {paper['Authors']} {paper['Abstract']} {paper['Subject_Tags']} {paper['Subjects']} {paper['Submitted Date']}"
-    return text_paper_metadata
+    return preprocess_text(text_paper_metadata)
 
